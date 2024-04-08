a44e1512464f7873caad51361fd0fbbeb91e91ec
sumin <suminshin211@gmail.com>
2024-04-08 17:42:01 +0900

    12일차 수업 내용

--- 12일차/test0099.js
+++ 12일차/test0099.js
@@ -7,3 +7,22 @@ console.log(total);
 let phone = ['010', '1234', '5678'];
 let phoneJoin = phone.join("-");
 console.log(phoneJoin);
+
+let num = [10, 20, 30];
+console.log(num.pop());
+console.log(num);
+
+let work = new Array();
+console.log(work);
+work.push("인터넷");
+console.log(work);
+work.push("카톡");
+console.log(work);
+work.push("유튜브");
+console.log(work);
+console.log(work.pop());
+console.log(work);
+console.log(work.pop());
+console.log(work);
+console.log(work.pop());
+console.log(work);
---
+++ 12일차/test0100.js
@@ -0,0 +1,10 @@
+// slice
+var num = [1, 2, 3, 4, 5 ];
+var newNum = num.slice(2);
+console.log(newNum);
+console.log(num);
+
+var num = [1, 2, 3, 4, 5 ];
+var newNum = num.slice(2, 4);
+console.log(newNum);
+console.log(num);
