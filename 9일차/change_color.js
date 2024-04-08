4371b0c3227affd77dee523eb7e339da75a5188d
sumin <suminshin211@gmail.com>
2024-04-08 10:04:35 +0900

    12일차 수업 내용

--- 9일차/change_color.js
+++ 9일차/change_color.js
@@ -1,5 +1,6 @@
-alert('안녕! 외부 자바스크립트!!');
-var heading = document.querySelector('#heading');
-heading.onclick = function() {
-    heading.style.color = "skyblue";
-}
+// alert('안녕! 외부 자바스크립트!!');
+// var heading = document.querySelector('#heading');
+// heading.onclick = function() {
+//     heading.style.color = "skyblue";
+// }
+console.log('안녕!!');
