26de652db769c5907927399dafc2558622408116
sumin <suminshin211@gmail.com>
2024-04-08 16:53:18 +0900

    12일차 수업 내용

---
+++ 12일차/test0098.js
@@ -0,0 +1,8 @@
+var numbers = new Array();
+var numbers2 = new Array(4);
+
+console.log("numbers 길이", numbers.length);
+console.log("numbers2 길이", numbers2.length);
+
+var numbers3 = ["one", "two", "three"];
+console.log("numbers3 길이", numbers3.length);
---
+++ 12일차/test0099.js
@@ -0,0 +1,9 @@
+let ban1 = [90, 80, 85];
+let ban2 = [79, 88, 93];
+
+let total = ban1.concat(ban2);
+console.log(total);
+
+let phone = ['010', '1234', '5678'];
+let phoneJoin = phone.join("-");
+console.log(phoneJoin);
